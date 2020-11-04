d244db43de1809c714de7c20baf13c9c70707943
amilcar-capsus <amilcar.gonzalez@capsus.mx>
2020-11-04 13:30:49 -0600

    Adjust keys

--- lib/views/questionnaires/targets/chkAction.dart
+++ lib/views/questionnaires/targets/chkAction.dart
@@ -11,27 +11,29 @@ import 'package:siap_monitoring/views/contestaCuestionario/pregunta.dart';
 import 'package:siap_monitoring/views/verCuestionario/verCuestionario.dart';
 
 class ChkAction extends StatefulWidget {
-
   Map datTE;
   Map datChk;
 
-  ChkAction({this.datTE,this.datChk});
+  ChkAction({this.datTE, this.datChk});
 
   @override
   ChkActionState createState() => ChkActionState();
 }
 
 class ChkActionState extends State<ChkAction> {
-    GlobalKey<BloquesBtnState> KeyBloques = GlobalKey();
-    GlobalKey<AreasState> KeyAreas = GlobalKey();
-    GlobalKey<PreguntasContState> KeyPreguntas = GlobalKey();
-    GlobalKey<PreguntaState> KeyPregunta = GlobalKey();
+  GlobalKey<ChkActionState> KeyChk = GlobalKey();
+  GlobalKey<BloquesBtnState> KeyBloques = GlobalKey();
+  GlobalKey<AreasState> KeyAreas = GlobalKey();
+  GlobalKey<PreguntasContState> KeyPreguntas = GlobalKey();
+  GlobalKey<PreguntaState> KeyPregunta = GlobalKey();
+
+  ChkActionState({this.KeyChk});
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
       future: getInfo(),
-      builder: (context,snapshot){
+      builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.none:
             return Text('');
@@ -39,7 +41,7 @@ class ChkActionState extends State<ChkAction> {
           case ConnectionState.waiting:
             return Text(Translations.of(context).text('waiting'));
           case ConnectionState.done:
-            if (snapshot.hasError){
+            if (snapshot.hasError) {
               return Text('Error: ${snapshot.error}');
             }
 //            print('Snapshot:${snapshot.data}');
@@ -53,12 +55,11 @@ class ChkActionState extends State<ChkAction> {
 //                border: Border.all(
 //                  color: Colors.black,
 //                )
-              ),
+                  ),
             );
           default:
             return Column();
         }
-
       },
     );
   }
@@ -79,160 +80,143 @@ class ChkActionState extends State<ChkAction> {
     List visita = await db.query(sql);
     visita ??= [];
 
-
-    if(visita.length == 0){
+    if (visita.length == 0) {
       return answerSurvey(
         checklistId: widget.datChk['checklistId'],
         elemId: widget.datTE['id'],
       );
-    }else{
+    } else {
       var vis = visita[0];
       DateTime nextDate;
       DateTime now = new DateTime.now();
-      switch(vis['fCode']){
+      switch (vis['fCode']) {
         case "oneTime":
-          nextDate = now.add(Duration(days:-100000));
+          nextDate = now.add(Duration(days: -100000));
           break;
         case "daily":
-          nextDate = now.add(Duration(days:-1));
+          nextDate = now.add(Duration(days: -1));
           break;
         case "weekly":
-          nextDate = now.add(Duration(days:-7));
+          nextDate = now.add(Duration(days: -7));
           break;
         case "2weeks":
-          nextDate = now.add(Duration(days:-14));
+          nextDate = now.add(Duration(days: -14));
           break;
         case "3weeks":
-          nextDate = now.add(Duration(days:-21));
+          nextDate = now.add(Duration(days: -21));
           break;
         case "monthly":
-          nextDate = now.add(Duration(days:-30));
+          nextDate = now.add(Duration(days: -30));
           break;
         case "2months":
-          nextDate = now.add(Duration(days:-60));
+          nextDate = now.add(Duration(days: -60));
           break;
         case "3months":
-          nextDate = now.add(Duration(days:-90));
+          nextDate = now.add(Duration(days: -90));
           break;
         case "4months":
-          nextDate = now.add(Duration(days:-120));
+          nextDate = now.add(Duration(days: -120));
           break;
         case "6months":
-          nextDate = now.add(Duration(days:-180));
+          nextDate = now.add(Duration(days: -180));
           break;
         case "yearly":
-          nextDate = now.add(Duration(days:-365));
+          nextDate = now.add(Duration(days: -365));
           break;
         default:
           break;
-
       }
 
-
-      if(vis['finalizada'] == null){
+      if (vis['finalizada'] == null) {
         return cont(
           checklistId: widget.datChk['checklistId'],
           vId: visita[0]['id'],
         );
       }
 
-
-      var finishDate = vis['finishDate']!= null?vis['finishDate'].split(' ')[0]:null;
+      var finishDate =
+          vis['finishDate'] != null ? vis['finishDate'].split(' ')[0] : null;
       DateTime fDate = DateTime.parse('$finishDate 00:00:00Z');
       String nextDateStr = '$nextDate'.split(' ')[0];
       nextDate = DateTime.parse("$nextDateStr 00:00:00Z");
 
-
-
-      if(vis['fCode'] == 'oneTime'){
-        return answered(finishDate: finishDate,vId: visita[0]['id']);
-      }else{
+      if (vis['fCode'] == 'oneTime') {
+        return answered(finishDate: finishDate, vId: visita[0]['id']);
+      } else {
         var difference = fDate.difference(nextDate).inDays;
 //        print('FinishDate: $fDate');
 //        print('nextDate: ${nextDate}');
 //        print('Difference: ${difference.runtimeType}');
-        if(difference <= 0){
+        if (difference <= 0) {
           return answerSurvey(
             checklistId: widget.datChk['checklistId'],
             elemId: widget.datTE['id'],
           );
         }
-        return answered(finishDate: finishDate,vId: visita[0]['id']);
+        return answered(finishDate: finishDate, vId: visita[0]['id']);
       }
     }
 
-    print('Visita chk[${widget.datChk['checklistId']}] uT[${widget.datTE['id']}]: $visita');
-
+    print(
+        'Visita chk[${widget.datChk['checklistId']}] uT[${widget.datTE['id']}]: $visita');
   }
 
-  Widget answered({String finishDate, int vId}){
+  Widget answered({String finishDate, int vId}) {
     return Column(
       children: <Widget>[
         Text(
           '${Translations.of(context).text('sended')}:${finishDate}',
-          style: TextStyle(
-              color: Colors.grey[600],
-              fontSize: 9
-          ),
+          style: TextStyle(color: Colors.grey[600], fontSize: 9),
         ),
         FlatButton(
           child: Text(
             '${Translations.of(context).text('seeResults')}',
-            style: TextStyle(
-              color: Colors.grey[600],
-              fontSize: 13
-            ),
+            style: TextStyle(color: Colors.grey[600], fontSize: 13),
           ),
-          onPressed: (){
-            Navigator.push(context,
+          onPressed: () {
+            Navigator.push(
+                context,
                 new MaterialPageRoute(
-                    builder: (context)=>VerCuestionario(vId)
-                )
-            );
+                    builder: (context) => VerCuestionario(vId)));
           },
-
         )
-
       ],
     );
   }
 
-  Widget answerSurvey({int elemId,int checklistId}){
+  Widget answerSurvey({int elemId, int checklistId}) {
     return FlatButton(
+      key: KeyChk,
       child: Text(
         '${Translations.of(context).text('answerSurvey')}',
-        style: TextStyle(
-            color: Colors.grey[600]
-        ),
+        style: TextStyle(color: Colors.grey[600]),
       ),
-      onPressed: (){
-        creaVisita(elemId: elemId,checklistId: checklistId);
+      onPressed: () {
+        creaVisita(elemId: elemId, checklistId: checklistId);
       },
     );
   }
 
-  Widget cont({int vId,int checklistId}){
+  Widget cont({int vId, int checklistId}) {
     return FlatButton(
       child: Text(
         '${Translations.of(context).text('continue')}',
-        style: TextStyle(
-            color: Colors.grey[600]
-        ),
+        style: TextStyle(color: Colors.grey[600]),
       ),
-      onPressed: (){
+      onPressed: () {
         goToSurvey(vId: vId);
       },
     );
   }
 
-  void creaVisita({int elemId,int checklistId}) async {
+  void creaVisita({int elemId, int checklistId}) async {
     DB db = DB.instance;
 
     DateTime now = new DateTime.now();
     String formattedDate = DateFormat('yyyy-MM-dd').format(now);
 //    print(formattedDate);
 
-    Map<String,dynamic> datVis = Map();
+    Map<String, dynamic> datVis = Map();
     datVis['timestamp'] = formattedDate;
     datVis['checklistId'] = checklistId;
     datVis['type'] = 'trgt';
@@ -242,20 +226,18 @@ class ChkActionState extends State<ChkAction> {
     var vId = await db.insert('Visitas', datVis, true);
 
     goToSurvey(vId: vId);
-
   }
 
   void goToSurvey({int vId}) async {
-
-    Navigator.push(context,
-        new MaterialPageRoute(builder: (context)=>ContestaCuestionario(
-          vId:vId,
-          KeyBloques: KeyBloques,
-          KeyAreas: KeyAreas,
-          KeyPreguntas: KeyPreguntas,
-          KeyPregunta:KeyPregunta,
-        )));
+    Navigator.push(
+        context,
+        new MaterialPageRoute(
+            builder: (context) => ContestaCuestionario(
+                  vId: vId,
+                  KeyBloques: KeyBloques,
+                  KeyAreas: KeyAreas,
+                  KeyPreguntas: KeyPreguntas,
+                  KeyPregunta: KeyPregunta,
+                )));
   }
-
-
 }
